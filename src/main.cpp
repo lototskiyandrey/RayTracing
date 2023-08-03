886b93657a044824e45508d147f6b62cbff6e7ca
Andrey Lototskiy <andrey.lototskiy@gmail.com>
2023-08-03 09:45:00 -0600

    made some quick changes

--- src/main.cpp
+++ src/main.cpp
@@ -58,29 +58,8 @@ int main(void){
     //omp_set_num_threads(THREAD_NUM);
 
     std::vector<std::vector<std::tuple<int, int, int>>> image(image_height, std::vector<std::tuple<int, int, int>>(image_width));
-
-    //#pragma omp parallel for
     
-    
-    for(int j = image_height-1; j >= 0; --j){
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for(int i = 0; i < image_width; ++i){
-            color pixel_color(0, 0, 0);
-            for(int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j +  random_double()) / (image_height -1);
-                ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
-            }
-            //#pragma omp critical
-                //write_color(std::cout, pixel_color, samples_per_pixel);
-            image = write_color_to_array(pixel_color, samples_per_pixel, image, i, j);
-        }
-    }
-    
-
-
-    //renderImage(image, image_height, image_width, world, samples_per_pixel, cam, max_depth);
+    renderImage(image, image_height, image_width, world, samples_per_pixel, cam, max_depth);
 
 
 
@@ -174,107 +153,9 @@ color ray_color(const ray &r, const hittable &world, int depth) {
 
 
 
-hittable_list random_scene() {
-    hittable_list world;
-
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5 ,0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
 
-    for(int a = -11; a < 11; a++) {
-        for(int b = -11; b < 11; b++) {
-            auto choose_mat = random_double();
-            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
-
-            if((center - point3(4, 0.2, 0)).length() > 0.9) {
-                shared_ptr<material> sphere_material;
-
-                if(choose_mat < 0.8) {
-                    // diffuse
-                    auto albedo = color::random() * color::random();
-                    sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-                else if(choose_mat < 0.95) {
-                    // metal
-                    auto albedo = color::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-                else {
-                    // glass
-                    sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-            }
-        }
-    }
-
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-
-    return world;
-}
 
 
-hittable_list simpler_random_scene() {
-    hittable_list world;
-
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5 ,0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
-
-        //#pragma omp parallel for
-
-    
-        /*for(int a = -3; a < 3; a++) {
-            for(int b = -3; b < 3; b++) {
-                auto choose_mat = random_double();
-                point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
-
-                if((center - point3(4, 0.2, 0)).length() > 0.9) {
-                    shared_ptr<material> sphere_material;
-
-                    if(choose_mat < 0.8) {
-                        // diffuse
-                        auto albedo = color::random() * color::random();
-                        sphere_material = make_shared<lambertian>(albedo);
-                        world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                    }
-                    else if(choose_mat < 0.95) {
-                        // metal
-                        auto albedo = color::random(0.5, 1);
-                        auto fuzz = random_double(0, 0.5);
-                        sphere_material = make_shared<metal>(albedo, fuzz);
-                        world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                    }
-                    else {
-                        // glass
-                        sphere_material = make_shared<dielectric>(1.5);
-                        world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                    }
-                }
-            }
-        }*/
-    
-
-
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-
-    return world;
-}
 
 
