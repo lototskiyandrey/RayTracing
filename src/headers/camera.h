b0158111ea9dc9030f0666bd5431e63ccc0a9b6e
Andriy <andrey.lototskiy@gmail.com>
2025-05-10 16:20:51 -0600

    Added Antialiasing

--- src/headers/camera.h
+++ src/headers/camera.h
@@ -8,6 +8,7 @@ class camera
     public:
         double aspect_ratio = 1.0;
         int image_width = 100;
+        int samples_per_pixel = 10;
 
         void render(const hittable &world) 
         {   
@@ -21,13 +22,13 @@ class camera
                 std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
                 for(int i = 0; i < image_width; i++)
                 {
-                    auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                    auto ray_direction = pixel_center - center;
-
-                    ray r(center, ray_direction);
-
-                    color pixel_color = ray_color(r, world);
-                    write_color(std::cout, pixel_color);
+                    color pixel_color(0,0,0);
+                    for(int sample = 0; sample < samples_per_pixel; sample++) 
+                    {
+                        ray r = get_ray(i,j);
+                        pixel_color += ray_color(r, world);
+                    }
+                    write_color(std::cout, pixel_samples_scale * pixel_color);
                 }
             }
             std::clog << "\rDone.                  \n";
@@ -35,6 +36,7 @@ class camera
 
     private:
         int image_height;
+        double pixel_samples_scale;
         point3 center;
         point3 pixel00_loc;
         vec3 pixel_delta_u;
@@ -44,6 +46,8 @@ class camera
         {
             image_height = int(image_width / aspect_ratio);
             image_height = (image_height < 1) ? 1 : image_height;
+
+            pixel_samples_scale = 1.0 / samples_per_pixel;
 
             center = point3(0,0,0);
 
@@ -78,6 +82,23 @@ class camera
             vec3 unit_direction = unit_vector(r.direction());
             auto a = 0.5 * (unit_direction.y() + 1.0);
             return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+        }
+
+        ray get_ray(int i, int j) const 
+        {
+            // Construct a camera ray originating from the origin and directed at randomly sampled
+            // point around the pixel location i, j.
+            auto offset = sample_square();
+            auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
+            auto ray_origin = center;
+            auto ray_direction = pixel_sample - ray_origin;
+
+            return ray(ray_origin, ray_direction);
+        }
+
+        vec3 sample_square() const 
+        {
+            return vec3(random_double() - 0.5, random_double() - 0.5, 0);
         }
 };
 
--- src/headers/rtweekend.h
+++ src/headers/rtweekend.h
@@ -20,6 +20,18 @@ inline double degrees_to_radians(double degrees)
     return degrees * pi / 180;
 }
 
+// Return a random real in [0,1)
+inline double random_double() 
+{
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+// Return a random real in [min, max)
+inline double random_double(double min, double max) 
+{
+    return min + (max - min) * random_double();
+}
+
 // Useful Headers
 #include "color.h"
 #include "interval.h"
