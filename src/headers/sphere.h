f1d23ef58cb8fff674f570813f448b96fdd88bde
Andriy <andrey.lototskiy@gmail.com>
2025-05-10 15:09:11 -0600

    Implemented interval.h

--- src/headers/sphere.h
+++ src/headers/sphere.h
@@ -2,14 +2,13 @@
 #define SPHERE_H
 
 #include "hittable.h"
-#include "vec3.h"
 
 class sphere : public hittable
 {
     public:
         sphere(const point3 &center, double radius): center(center), radius(std::fmax(0,radius)) {}
 
-        bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override
+        bool hit(const ray &r, interval ray_t, hit_record &rec) const override
         {
             vec3 oc = center - r.origin();
             auto a = r.direction().length_squared();
@@ -25,10 +24,10 @@ class sphere : public hittable
 
             // Find the nearest root that lies in the acceptable range.
             auto root = (h - sqrtd) / a;
-            if(root <= ray_tmin || root >= ray_tmax)
+            if(!ray_t.surrounds(root))
             {
                 root = (h + sqrtd) / a;
-                if(root <= ray_tmin || root >= ray_tmax)
+                if(!ray_t.surrounds(root))
                 {
                     return false;
                 }
