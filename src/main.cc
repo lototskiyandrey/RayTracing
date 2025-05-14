6219688ce4990dccff2a1df1913b0c8268308783
Andriy <andrey.lototskiy@gmail.com>
2025-05-14 12:26:14 -0600

    Minor Renaming

--- src/main.cc
+++ src/main.cc
@@ -64,8 +64,8 @@ int main()
     camera cam;
 
     cam.aspect_ratio = 16.0/9.0;
-    cam.image_width = 1000;
-    cam.samples_per_pixel = 500;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
     cam.vfov = 20;
