337d76acc069b4d4996589ad3460051a361d4a59
Andriy <andrey.lototskiy@gmail.com>
2025-05-27 23:54:19 -0600

    Added uniform hemispherical sampling

--- src/headers/material.h
+++ src/headers/material.h
@@ -34,7 +34,7 @@ class lambertian : public material
 
         bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override 
         {
-            auto scatter_direction = rec.normal + random_unit_vector();
+            auto scatter_direction = random_on_hemisphere(rec.normal);
 
             if(scatter_direction.near_zero())
             {
@@ -48,8 +48,7 @@ class lambertian : public material
 
         double scattering_pdf(const ray &r_in, const hit_record &rec, const ray &scattered) const override 
         {
-            auto cos_theta = dot(rec.normal, unit_vector(scattered.direction()));
-            return cos_theta < 0 ? 0 : cos_theta / pi;
+            return 1 / (2 * pi);
         }
 
     private:
