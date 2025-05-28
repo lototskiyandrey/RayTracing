b0691441f4af3c2fe0d885d79f8a6e845f2886c9
Andriy <andrey.lototskiy@gmail.com>
2025-05-28 10:42:03 -0600

    Added light sampling

--- src/main.cc
+++ src/main.cc
@@ -77,7 +77,7 @@ void simple_cornell_box()
 
     cam.aspect_ratio      = 1.0;
     cam.image_width       = 600;
-    cam.samples_per_pixel = 500;
+    cam.samples_per_pixel = 10;
     cam.max_depth         = 50;
     cam.background        = color(0,0,0);
 
