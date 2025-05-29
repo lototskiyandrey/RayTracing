b7b87037ffadc8c1bfbf79dd2096201969b6db91
Andriy <andrey.lototskiy@gmail.com>
2025-05-29 08:52:12 -0600

    Updated the sphere code

--- src/main.cc
+++ src/main.cc
@@ -106,18 +106,15 @@ void metal_cornell_box()
     // Light
     world.add(make_shared<quad>(point3(213,554,227), vec3(130,0,0), vec3(0,0,105), light));
 
-    // Box 1
-    shared_ptr<material> aluminum = make_shared<metal>(color(0.8, 0.85, 0.88), 0.0);
-    shared_ptr<hittable> box1 = box(point3(0,0,0), point3(165,330,165), aluminum);
+    // Box
+    shared_ptr<hittable> box1 = box(point3(0,0,0), point3(165,330,165), white);
     box1 = make_shared<rotate_y>(box1, 15);
     box1 = make_shared<translate>(box1, vec3(265,0,295));
     world.add(box1);
 
-    // Box 2
-    shared_ptr<hittable> box2 = box(point3(0,0,0), point3(165,165,165), white);
-    box2 = make_shared<rotate_y>(box2, -18);
-    box2 = make_shared<translate>(box2, vec3(130,0,65));
-    world.add(box2);
+    // Glass Sphere
+    auto glass = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(190,90,190), 90, glass));
 
     // Light Sources
     auto empty_material = shared_ptr<material>();
@@ -125,16 +122,16 @@ void metal_cornell_box()
 
     camera cam;
 
-    cam.aspect_ratio      = 1.0;
-    cam.image_width       = 600;
+    cam.aspect_ratio = 1.0;
+    cam.image_width = 600;
     cam.samples_per_pixel = 100;
-    cam.max_depth         = 50;
-    cam.background        = color(0,0,0);
+    cam.max_depth = 50;
+    cam.background = color(0,0,0);
 
-    cam.vfov     = 40;
+    cam.vfov = 40;
     cam.lookfrom = point3(278, 278, -800);
-    cam.lookat   = point3(278, 278, 0);
-    cam.vup      = vec3(0, 1, 0);
+    cam.lookat = point3(278, 278, 0);
+    cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0;
 
